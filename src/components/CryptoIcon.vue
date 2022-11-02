b6975403d1a80bb4604a9a35db86d854241fc334
Your Name <you@example.com>
2022-11-02 17:59:59 +0000

    latest

---
+++ src/components/CryptoIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    const props = defineProps({
+        icon: String
+    })
+    let url = new URL(`../assets/crypto-icons/${props.icon}.svg`, import.meta.url).href;
+</script>
+
+<template>
+    <v-img class="crypto-icon" style="margin: 0 auto;" :src="url"></v-img>
+</template>
+
+<style>
+.crypto-icon > img {
+    position: relative !important;
+}
+</style>
